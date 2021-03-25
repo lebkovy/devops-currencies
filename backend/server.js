dd6faeb4aa2d4d1740b922bf1ebcffa16674cd66
lebkovy <Tima2305>
2021-03-25 17:19:27 +0100

    Add redis client

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const morgan = require('morgan')
+const redis = require('redis')
 
 const db = require('./db')
 
 const PORT = process.env.PORT || 5000
 const app = express()
+
+const redisClient = redis.createClient({
+  host: "redis",
+  port: 6379
+})
+
+redisClient.on('connect', () => {
+  console.log('Connected to the Redis server');
+})
 
 app.use(morgan('dev'))
 app.use(express.json())
